53294ebb223c8284cb0a2a8491c67cba011f5832
Daniel Rivaldo <87303588+DanielRivaldo3@users.noreply.github.com>
2022-05-22 21:50:37 +0700

    Add files via upload

---
+++ Music_Player/Music_Player/Form1.Designer.vb
@@ -0,0 +1,169 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.AxWindowsMediaPlayer1 = New AxWMPLib.AxWindowsMediaPlayer()
+        Me.rb_pause = New System.Windows.Forms.RadioButton()
+        Me.btn_open = New System.Windows.Forms.Button()
+        Me.btn_play = New System.Windows.Forms.Button()
+        Me.btn_stop = New System.Windows.Forms.Button()
+        Me.rb_resume = New System.Windows.Forms.RadioButton()
+        Me.track_bar_volume = New System.Windows.Forms.TrackBar()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.AxWindowsMediaPlayer2 = New AxWMPLib.AxWindowsMediaPlayer()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        CType(Me.AxWindowsMediaPlayer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.track_bar_volume, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.AxWindowsMediaPlayer2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'AxWindowsMediaPlayer1
+        '
+        Me.AxWindowsMediaPlayer1.Enabled = True
+        Me.AxWindowsMediaPlayer1.Location = New System.Drawing.Point(32087, 32071)
+        Me.AxWindowsMediaPlayer1.Name = "AxWindowsMediaPlayer1"
+        Me.AxWindowsMediaPlayer1.OcxState = CType(resources.GetObject("AxWindowsMediaPlayer1.OcxState"), System.Windows.Forms.AxHost.State)
+        Me.AxWindowsMediaPlayer1.Size = New System.Drawing.Size(8, 87)
+        Me.AxWindowsMediaPlayer1.TabIndex = 1
+        '
+        'rb_pause
+        '
+        Me.rb_pause.AutoSize = True
+        Me.rb_pause.Font = New System.Drawing.Font("Candara", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rb_pause.Location = New System.Drawing.Point(285, 15)
+        Me.rb_pause.Name = "rb_pause"
+        Me.rb_pause.Size = New System.Drawing.Size(91, 33)
+        Me.rb_pause.TabIndex = 3
+        Me.rb_pause.TabStop = True
+        Me.rb_pause.Text = "Pause"
+        Me.rb_pause.UseVisualStyleBackColor = True
+        '
+        'btn_open
+        '
+        Me.btn_open.Font = New System.Drawing.Font("Candara", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_open.Location = New System.Drawing.Point(12, 12)
+        Me.btn_open.Name = "btn_open"
+        Me.btn_open.Size = New System.Drawing.Size(70, 36)
+        Me.btn_open.TabIndex = 6
+        Me.btn_open.Text = "Open"
+        Me.btn_open.UseVisualStyleBackColor = True
+        '
+        'btn_play
+        '
+        Me.btn_play.Font = New System.Drawing.Font("Candara", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_play.Location = New System.Drawing.Point(106, 12)
+        Me.btn_play.Name = "btn_play"
+        Me.btn_play.Size = New System.Drawing.Size(70, 36)
+        Me.btn_play.TabIndex = 7
+        Me.btn_play.Text = "Play"
+        Me.btn_play.UseVisualStyleBackColor = True
+        '
+        'btn_stop
+        '
+        Me.btn_stop.Font = New System.Drawing.Font("Candara", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_stop.Location = New System.Drawing.Point(198, 12)
+        Me.btn_stop.Name = "btn_stop"
+        Me.btn_stop.Size = New System.Drawing.Size(70, 36)
+        Me.btn_stop.TabIndex = 8
+        Me.btn_stop.Text = "Stop"
+        Me.btn_stop.UseVisualStyleBackColor = True
+        '
+        'rb_resume
+        '
+        Me.rb_resume.AutoSize = True
+        Me.rb_resume.Font = New System.Drawing.Font("Candara", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rb_resume.Location = New System.Drawing.Point(382, 15)
+        Me.rb_resume.Name = "rb_resume"
+        Me.rb_resume.Size = New System.Drawing.Size(112, 33)
+        Me.rb_resume.TabIndex = 9
+        Me.rb_resume.TabStop = True
+        Me.rb_resume.Text = "Resume"
+        Me.rb_resume.UseVisualStyleBackColor = True
+        '
+        'track_bar_volume
+        '
+        Me.track_bar_volume.Location = New System.Drawing.Point(12, 83)
+        Me.track_bar_volume.Maximum = 100
+        Me.track_bar_volume.Name = "track_bar_volume"
+        Me.track_bar_volume.Size = New System.Drawing.Size(498, 45)
+        Me.track_bar_volume.TabIndex = 10
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Candara", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 61)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(61, 19)
+        Me.Label1.TabIndex = 11
+        Me.Label1.Text = "Volume"
+        '
+        'AxWindowsMediaPlayer2
+        '
+        Me.AxWindowsMediaPlayer2.Enabled = True
+        Me.AxWindowsMediaPlayer2.Location = New System.Drawing.Point(12, 134)
+        Me.AxWindowsMediaPlayer2.Name = "AxWindowsMediaPlayer2"
+        Me.AxWindowsMediaPlayer2.OcxState = CType(resources.GetObject("AxWindowsMediaPlayer2.OcxState"), System.Windows.Forms.AxHost.State)
+        Me.AxWindowsMediaPlayer2.Size = New System.Drawing.Size(491, 149)
+        Me.AxWindowsMediaPlayer2.TabIndex = 12
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(518, 294)
+        Me.Controls.Add(Me.AxWindowsMediaPlayer2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.track_bar_volume)
+        Me.Controls.Add(Me.rb_resume)
+        Me.Controls.Add(Me.btn_stop)
+        Me.Controls.Add(Me.btn_play)
+        Me.Controls.Add(Me.btn_open)
+        Me.Controls.Add(Me.rb_pause)
+        Me.Controls.Add(Me.AxWindowsMediaPlayer1)
+        Me.Name = "Form1"
+        Me.Text = "Music Player by Daniel"
+        CType(Me.AxWindowsMediaPlayer1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.track_bar_volume, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.AxWindowsMediaPlayer2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents AxWindowsMediaPlayer1 As AxWMPLib.AxWindowsMediaPlayer
+    Friend WithEvents rb_pause As System.Windows.Forms.RadioButton
+    Friend WithEvents btn_open As System.Windows.Forms.Button
+    Friend WithEvents btn_play As System.Windows.Forms.Button
+    Friend WithEvents btn_stop As System.Windows.Forms.Button
+    Friend WithEvents rb_resume As System.Windows.Forms.RadioButton
+    Friend WithEvents track_bar_volume As System.Windows.Forms.TrackBar
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents AxWindowsMediaPlayer2 As AxWMPLib.AxWindowsMediaPlayer
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+
+End Class
---
+++ Music_Player/Music_Player/Form1.vb
@@ -0,0 +1,31 @@
+﻿Public Class Form1
+
+    Private Sub btn_open_Click(sender As Object, e As EventArgs) Handles btn_open.Click
+        OpenFileDialog1.ShowDialog()
+
+    End Sub
+
+    Private Sub btn_play_Click(sender As Object, e As EventArgs) Handles btn_play.Click
+        AxWindowsMediaPlayer2.URL = OpenFileDialog1.FileName
+        AxWindowsMediaPlayer2.Ctlcontrols.play()
+
+    End Sub
+
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles rb_pause.CheckedChanged
+        AxWindowsMediaPlayer2.Ctlcontrols.pause()
+
+    End Sub
+
+    Private Sub btn_stop_Click(sender As Object, e As EventArgs) Handles btn_stop.Click
+        AxWindowsMediaPlayer2.Ctlcontrols.stop()
+
+    End Sub
+
+    Private Sub RadioButton2_CheckedChanged(sender As Object, e As EventArgs) Handles rb_resume.CheckedChanged
+        AxWindowsMediaPlayer2.Ctlcontrols.play()
+    End Sub
+
+    Private Sub track_bar_volume_Scroll(sender As Object, e As EventArgs) Handles track_bar_volume.Scroll
+        AxWindowsMediaPlayer2.settings.volume = track_bar_volume.Value
+    End Sub
+End Class
